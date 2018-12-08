465f5ded575c46658b7cc944ea55ee92c756ab4f
saber2pr <saber2pr@gmail.com>
2018-12-08 23:34:59 +0800

    update

--- main.cpp
+++ main.cpp
@@ -4,21 +4,35 @@
 
 using namespace std;
 
-void listener(int state){
-    Console::log(state);
+void listener1(int state){
+    Console::log("listener1: ",state);
+}
+
+void listener2(int state){
+    Console::log("listener2: ",state);
+}
+
+void listener3(int state){
+    Console::log("listener3: ",state);
 }
 
 int main()
 {
-    auto store = Store();
+    Store<int> store = Store<int>(233);
 
-    Console::log("state: " ,store.getState());
+    //    Console::log("state: " ,store.getState());
 
-    store.subscribe(listener);
+    auto unListener1 = store.subscribe(listener1);
+    auto unListener2 = store.subscribe(listener2);
+    auto unListener3 = store.subscribe(listener3);
+
+    unListener1();
+        unListener2();
+    //    unListener3();
 
     store.dispatch(233);
 
-    Console::log("state: " ,store.getState());
+    //    Console::log("state: " ,store.getState());
 
     return 0;
 }
--- src/store/store.h
+++ src/store/store.h
@@ -12,17 +12,40 @@ typedef function<void(int)> Listener;
 
 typedef function<void()> unsubscribe;
 
+template<typename T >
 class Store
 {
 private:
-    int state;
+    T state;
     vector<Listener> Listeners;
 public:
-    Store(int state = 0);
-    ~Store();
-    unsubscribe subscribe(Listener );
-    void dispatch(int);
-    int getState();
+    Store(T state){
+        this->state = state;
+    }
+
+    ~Store(){
+        this->Listeners.clear();
+    }
+
+    unsubscribe subscribe(Listener listener){
+        this->Listeners.push_back(listener);
+        int index = this->Listeners.size() - 1;
+        return [=](){
+            this->Listeners.erase(this->Listeners.begin() + index);
+            cout << index << endl;
+        };
+    }
+
+    void dispatch(T value){
+        this->state = value;
+        for_each(this->Listeners.begin(), this->Listeners.end(), [=](Listener listener){
+            listener(this->state);
+        });
+    }
+
+    T getState(){
+        return this->state;
+    }
 };
 
 #endif // STORE_H
